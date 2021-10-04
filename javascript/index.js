d4f289d489b6a3031909db08e2a7fcb7183d9c18
Omkar Bhosale <omkar.bhosale0369@gmail.com>
2021-10-04 17:46:08 +0530

    js added

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,27 @@
+var m=document.getElementById("mass_form");
+var l=document.getElementById("length_form");
+var c=document.getElementById("curr_form");
+function mass_form()
+{
+    m.style.transform="translatex(660px)";
+    l.style.transform="translatex(-660px)";
+    c.style.transform="translatex(0px)";
+
+}
+function length_form()
+{
+    m.style.transform="translatex(0px)";
+    l.style.transform="translatex(0px)";
+    c.style.transform="translatex(0px)";
+
+}
+function curr_form()
+{
+    m.style.transform="translatex(0px)";
+    l.style.transform="translatex(-330px)";
+    c.style.transform="translatex(330px)";
+
+}
 function insert(num) {
     document.form.textbox.value += num;
 }
